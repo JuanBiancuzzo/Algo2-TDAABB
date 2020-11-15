d407f782bf2d24fdf65df2fe9fb1dfbc649f9d57
Juan <jbiancuzzo@fi.uba.ar>
2020-11-15 07:02:33 -0300

    Agregando archivos necesarios
    Necesitaba hacer un makefile, y un archivo de pruebas, para las pruebas
    necesitaba el archivo pa2mm.h

--- abb.c
+++ abb.c
@@ -1,3 +1,48 @@
 #include <stdlib.h>
 
 #include "abb.h"
+
+
+abb_t* arbol_crear(abb_comparador comparador, abb_liberar_elemento destructor) {
+    return NULL;
+}
+
+int arbol_insertar(abb_t* arbol, void* elemento) {
+    return 0;
+}
+
+int arbol_borrar(abb_t* arbol, void* elemento) {
+    return 0;
+}
+
+void* arbol_buscar(abb_t* arbol, void* elemento) {
+    return NULL;
+}
+
+void* arbol_raiz(abb_t* arbol) {
+    return NULL;
+}
+
+bool arbol_vacio(abb_t* arbol) {
+    return true;
+}
+
+size_t arbol_recorrido_inorden(abb_t* arbol, void** array, size_t tamanio_array) {
+    return 0;
+}
+
+size_t arbol_recorrido_preorden(abb_t* arbol, void** array, size_t tamanio_array) {
+    return 0;
+}
+
+size_t arbol_recorrido_postorden(abb_t* arbol, void** array, size_t tamanio_array) {
+    return 0;
+}
+
+void arbol_destruir(abb_t* arbol) {
+    return;
+}
+
+size_t abb_con_cada_elemento(abb_t* arbol, int recorrido, bool (*funcion)(void*, void*), void* extra) {
+    return 0;
+}
---
+++ pruebas.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+
+#include "abb.h"
+#include "pa2mm.h"
+
+int main() {
+
+    return 0;
+}
